a5ea815a7751a5a9212ed8510f90c1cfb971117c
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-20 05:59:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,13 @@
-name = 'Edmwin'
-language = 'Python'
 def introduction(name)
     "Hi, my name is #{name}."
 end
+introduction("Josh")
 
 def introduction_with_language(name,language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_language("Dan", "Ember.js")
 
-def introduction_with_language_optional(name, language ='Ruby')
+def introduction_with_language_optional(name, language ="Ruby")
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
